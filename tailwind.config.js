f13b03905cb28b5a7e7048d8294c730e15347dd7
Caio Padilha <caiocfpadilha@gmail.com>
2024-03-30 13:25:24 -0300

    Eslint and Prettier included and configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.jsx",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.jsx'],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
